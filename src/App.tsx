e5d146d480321bae08d11bdda068794f9f0bbf0f
Alef Junio <alephjunio@gmail.com>
2020-11-20 18:50:02 -0300

    chore:  input autocomplete on

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+import ToastContainer from './Components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
